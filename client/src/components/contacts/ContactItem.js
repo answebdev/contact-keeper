8db052128ab4f0b387202aa9befc1f296a7e4be9
ANS <36783010+answebdev@users.noreply.github.com>
2020-02-08 11:45:02 -0800

    added Edit and Delete buttons to ContactItem component

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const ContactItem = ({ contact }) => {
   const { id, name, email, phone, type } = contact;
@@ -8,6 +9,7 @@ const ContactItem = ({ contact }) => {
       <h3 className='text-primary text-left'>
         {name}{' '}
         <span
+          style={{ float: 'right' }}
           className={
             'badge ' +
             (type === 'professional' ? 'badge-success' : 'badge-primary')
@@ -16,8 +18,28 @@ const ContactItem = ({ contact }) => {
           {type.charAt(0).toUpperCase() + type.slice(1)}
         </span>
       </h3>
+      <ul className='list'>
+        {email && (
+          <li>
+            <i class='fas fa-envelope-open'></i> {email}
+          </li>
+        )}
+        {phone && (
+          <li>
+            <i className='fas fa-phone'></i> {phone}
+          </li>
+        )}
+      </ul>
+      <p>
+        <button className='btn btn-dark btn-sm'>Edit</button>
+        <button className='btn btn-danger btn-sm'>Delete</button>
+      </p>
     </div>
   );
+};
+
+ContactItem.propTypes = {
+  contact: PropTypes.object.isRequired
 };
 
 export default ContactItem;
