1cac9ca4bd47aaccf4f502ad068f74a608bc22dd
ANS <36783010+answebdev@users.noreply.github.com>
2020-04-04 19:15:50 -0700

    updated server.js

--- server.js
+++ server.js
@@ -27,4 +27,6 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server started on Port ${PORT}`);
+});
